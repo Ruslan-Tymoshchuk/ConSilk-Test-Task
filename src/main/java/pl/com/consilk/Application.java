c96eba3928aa0fc3673c8b3ad2981571cdfca8bd
R-Tim <R-Tim@host.docker.internal>
2024-04-04 20:00:36 +0200

    implemented result formatter

---
+++ src/main/java/pl/com/consilk/Application.java
@@ -0,0 +1,9 @@
+package pl.com.consilk;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Formatter formatter = new Formatter();
+        System.out.println(formatter.format(6));
+    }
+}
